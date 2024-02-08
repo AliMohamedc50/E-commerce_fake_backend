696f8195341abfffad505ea53da88e2dbf86f18d
cass <alicastilo1097@gmail.com>
2024-02-08 13:28:44 +0200

    df

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,80 +1,48 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
 import Typography from '@mui/material/Typography'
-import { Button } from '@mui/material'
+import { Box, Button } from '@mui/material'
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import BalanceIcon from "@mui/icons-material/Balance";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import { useParams } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
 const Product = () => {
+      const upload_url = "http://localhost:1337";
 
-  const [selectImg, setSelectImg] = useState(0)
+  const id = useParams().id
+  const { data, loading, errorr } = useFetch(`/products/${id}?populate=*`);
+  const [selectImg, setSelectImg] = useState("img")
   const [quantity, setQuantity] = useState(1)
 
- const data = [
-   {
-     id: 1,
-     img: "https://images.pexels.com/photos/18553528/pexels-photo-18553528/free-photo-of-man-with-a-stubble-standing-with-his-arm-reached-out.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-     img2: "https://images.pexels.com/photos/18553532/pexels-photo-18553532/free-photo-of-man-with-a-stubble-sitting-at-the-table.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-     title: "for test",
-     isnew: false,
-     oldprice: "19",
-     price: "12",
-   },
-   {
-     id: 2,
-     img: "https://images.pexels.com/photos/15952334/pexels-photo-15952334/free-photo-of-young-man-with-dyed-platinum-hair-in-a-beige-t-shirt.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-     img2: "https://images.pexels.com/photos/15952331/pexels-photo-15952331/free-photo-of-young-man-with-dyed-platinum-hair-in-a-beige-t-shirt.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-     title: "for test",
-     isnew: true,
-     oldprice: "19",
-     price: "12",
-   },
-   {
-     id: 3,
-     img: "https://images.pexels.com/photos/1188748/pexels-photo-1188748.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-     img2: "https://images.pexels.com/photos/11569311/pexels-photo-11569311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-     title: "for test",
-     isnew: false,
-     oldprice: "19",
-     price: "12",
-   },
-   {
-     id: 4,
-     img: "https://images.pexels.com/photos/12730638/pexels-photo-12730638.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-     img2: "https://images.pexels.com/photos/16060730/pexels-photo-16060730/free-photo-of-man-wearing-a-casual-trendy-outfit-and-sunglasses.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-     title: "for test",
-     isnew: true,
-     oldprice: "19",
-     price: "12",
-   },
- ];
+console.log(selectImg);
   return (
-    <div className="product w-10/12 flex gap-10 flex-1 m-auto">
+    <div className="product relative top-28 w-10/12 flex gap-10 flex-1 m-auto">
       <div className="left flex flex-1 gap-4">
         <div className="left  flex flex-col	gap-3 ">
           <img
-            src={data[0].img}
+            src={upload_url + data?.attributes?.img?.data?.attributes?.url}
             className="w-32 cursor-pointer object-cover h-36"
             alt=""
-            onClick={() => setSelectImg(0)}
+            onClick={() => setSelectImg("img")}
           />
           <img
-            src={data[1].img}
+            src={upload_url + data?.attributes?.img2?.data?.attributes?.url}
             className="w-32 cursor-pointer object-cover h-36"
             alt=""
-            onClick={() => setSelectImg(1)}
+            onClick={() => setSelectImg("img2")}
           />
         </div>
-        <div className="right w-full">
-          <img className='w-full h-full' src={data[selectImg].img} alt="" />
-        </div>
+        <Box sx={{height: "80vh", overflow: "hidden"}} className="right w-full ">
+          <img className='w-full h-full object-cover' src={upload_url + data?.attributes?.[selectImg]?.data?.attributes?.url} alt="" />
+        </Box>
       </div>
       <div className="right flex-1 ">
         <Typography className="title" variant="h3" color="initial">
-          {data[1].title}
+          {/* {data[1].title} */}uiui
         </Typography>
-        <h3 className="text-blue-500 text-2xl my-5">${data[1].price}</h3>
-
+        {/* <h3 className="text-blue-500 text-2xl my-5">${data[1].price}</h3> */}
+        uiyuiy
         <Typography className="title" variant="p" color="initial">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate
           non quae exercitationem provident sequi, consequuntur reiciendis
